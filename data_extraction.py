be25b2963d2c916ecd19e4caf0224ce913a41463
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-02-19 16:50:26 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,25 +1,3 @@
-#import boto3
-#import os
-#
-## os.environ["AWS_ACCESS_KEY_ID"]="your access key"
-## os.environ["AWS_SECRET_ACCESS_KEY"]="your secret key"
-#
-#
-#def extract_data():
-#
-#    s3 = boto3.client('s3')
-#    bucket_name = 'stock-market-usecase'
-#    url = s3.generate_presigned_url(
-#                    ClientMethod='get_object',
-#                    Params={'Bucket': bucket_name, 'Key': 'mall_dataset.zip'},
-#                    ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
-#                )
-#    print(url)
-#    return url
-#
-#extract_data()
-
-
 import boto3
 import os
 
@@ -29,14 +7,14 @@ import os
 
 def extract_data():
 
-    s3 = boto3.client('s3')
-    bucket_name = 'deeplearning-mlops'
-    url = s3.generate_presigned_url(
-                    ClientMethod='get_object',
-                    Params={'Bucket': bucket_name, 'Key': 'object_detection_data.zip'},
-                    ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
-                )
-    print(url)
-    return url
+   s3 = boto3.client('s3')
+   bucket_name = 'stock-market-usecase'
+   url = s3.generate_presigned_url(
+                   ClientMethod='get_object',
+                   Params={'Bucket': bucket_name, 'Key': 'mall_dataset.zip'},
+                   ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
+               )
+   print(url)
+   return url
 
 extract_data()
